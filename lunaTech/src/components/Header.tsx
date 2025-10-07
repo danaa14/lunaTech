99c49143adfc1137821ee81111ae7edf3644b428
Patricia <trishawx008@gmail.com>
2025-10-07 21:33:42 +0300

    First header styling changes

--- lunaTech/src/components/Header.tsx
+++ lunaTech/src/components/Header.tsx
@@ -1,15 +1,21 @@
 import NavBar from './NavBar.tsx'
+import styles from './Header.module.css'
 
 const Header = () => {
 
     return (
-        <header>
+        
+        <header className= {styles.header}>
+            <div className={styles.topBar}>
+            <p>Welcome to Luna Tech!</p>
+        </div>
             <h1>Luna Tech</h1>
             <NavBar />
             <a href="#"><img src="/search.png" alt="Search" /></a>
             {/* <router-link to="/cart"> */}
                 <a href="#"><img src="/cart.png" alt="Cart" /></a>
             {/* </router-link> */}
+            <br/>
         </header>
     )
 }
--- lunaTech/src/components/NavBar.tsx
+++ lunaTech/src/components/NavBar.tsx
@@ -1,8 +1,9 @@
+import styles from './NavBar.module.css'
 const NavBar = () => {
 
     return (
         <nav>
-            <ul>
+            <ul className={styles.navbar}>
                 <li><a href="">Home</a></li>
                 <li><a href="">Products</a></li>
                 <li><a href="">About Us</a></li>
